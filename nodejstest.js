5c702e9018a3a820c183927db4a2f05ee46028b9
Daniel Lundgren <lundgren.daniel@gmail.com>
2018-10-09 16:51:37 +0200

    Dirty hack works

--- nodejstest.js
+++ nodejstest.js
@@ -6,17 +6,6 @@ console.log("Parsing for fun");
 fs.readFile('replays/liquid-vs-astralis-m3-cache.dem', function (err, buffer) {
   let demoFile = new demofile.DemoFile();
 
-  /*demoFile.gameEvents.on('player_death', e => {
-    console.log("A gameevent is triggered");
-    let players = demoFile.entities.players;
-    for (var i = 0; i < players.length; i++) {
-    	var player = players[i];
-    	console.log(`Tick: ${player.name} @ ${player.position.x}, ${player.position.y}`);	
-    }
-    
-  }); */
-// Plot between 5-7 seconds
-
   let roundstart = false;
   demoFile.gameEvents.on('round_start', e => {
     console.log("A round has been started at %d", demoFile.currentTime);
@@ -25,18 +14,20 @@ fs.readFile('replays/liquid-vs-astralis-m3-cache.dem', function (err, buffer) {
   });
 
   demoFile.on('tickend', e => {
-  	console.log("=============================");
+  	// console.log("=============================");
   	if (roundstart) {
   		let roundTime = demoFile.currentTime - roundStartTime;
-  		if (roundTime > 7) roundstart = false;
-  		if (roundstart && roundTime > 5) {
+  		if (roundTime > 7 + 15) roundstart = false;
+  		if (roundstart && roundTime > 5 + 15) {
   			let teams = demoFile.teams;
   			// Team 3 is CT
 		  	let cts = teams[3].members;
 		    for (var i = 0; i < cts.length; i++) {
-		    	var pos = cts[i].position;
-		    	console.log("%f,%f,%f",
-		    		roundTime, pos.x, pos.y);
+		    	let ct = cts[i];
+		    	let pos = ct.position;
+		    	process.stdout.write(roundTime + "," + pos.x);
+		    	process.stdout.write("," + pos.y);
+		    	process.stdout.write("\n");	
 		    }
   		}
   	}
