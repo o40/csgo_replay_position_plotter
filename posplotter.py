c3d91c908fea88ec2df669174daf0e2100edb296
Daniel Lundgren <lundgren.daniel@gmail.com>
2018-10-12 10:16:06 +0200

    WIP

--- posplotter.py
+++ posplotter.py
@@ -2,56 +2,105 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-# plt.style.use('Solarize_Light2')
+import sys
+import getopt
+import collections
+
+RadarData = collections.namedtuple('RadarData',
+                                   'image extent plotarea bangpos bangang')
+
+if len(sys.argv) != 3:
+    print("Usage: posplotter.py [mapname] [csv file name]")
+    exit()
+
+mapname = sys.argv[1]
+csv_file = sys.argv[2]
 
 xcoords = []
 ycoords = []
-# roundTimeOffset = 15
+
 imagecount = 0
-radar_x_pos = -2000
-radar_y_pos = 3250
-radar_size = 5650 # Scale 5.5
-radar_extent = [radar_x_pos, radar_x_pos + radar_size, radar_y_pos - radar_size, radar_y_pos]
 frameskip = 64
 frame = 0
 ref_tick = 0
 
-wallbang_player_pos_x = [3309]
-wallbang_player_pos_y = [69]
 
-# Angle is -179.615616, assume 180
-filename = 'all.csv'
-# filename = 'nodejstest.csv'
-# filename = 'nodejstest_utf8.csv'
+def make_radar_extent(pos_x, pos_y, size):
+    '''
+    X, Y is the top left of the radar image (see radar config)
+    size is the size of the radar. I have not figured out
+    how to read the scale value in the radar config.
+
+    See: ..csgo/resource/overviews/de_cache.txt
+    '''
+    return [pos_x, pos_x + size, pos_y - size, pos_y]
+
+
+def get_radar_data(mapname):
+    if mapname == "de_cache":
+        # "pos_x"     "-2000" // upper left world coordinate
+        # "pos_y"     "3250"
+        # "scale"     "5.5"
+        return RadarData(mapname + "_radar.png",
+                         make_radar_extent(-2000, 3250, 1024 * 5.5),
+                         [-1000, 0, -500, 500],
+                         [3309, 69],
+                         180)
+    elif mapname == "de_overpass":
+        # "pos_x"     "-4831" // upper left world coordinate
+        # "pos_y"     "1781"
+        # "scale"     "5.2"
+        return RadarData(mapname + "_radar.png",
+                         make_radar_extent(-4831, 1781, 1024 * 5.2),
+                         [-2300, -1300, -500, 500],
+                         [3309, 69],
+                         180)
+    elif mapname == "de_inferno":
+        # "pos_x"     "-2087" // upper left world coordinate
+        # "pos_y"     "3870"
+        # "scale"     "4.9"
+        return RadarData(mapname + "_radar.png",
+                         make_radar_extent(-2087, 3870, 1024 * 4.9),
+                         [-2300, -1300, -500, 500],
+                         [0, 0],
+                         0)
+    else:
+        print(mapname, "not supported")
+        exit()
+
+
+radar_data = get_radar_data(mapname)
 
 
 def tickToRoundTime(tick):
     # 1:55 round time
     # Tick includes 15 seconds buy time
-    roundSeconds = 1 * 60 + 55 + 15
+    roundSeconds = 1 * 60 + 55
     secondsLeftInRound = math.floor(roundSeconds - (int(tick) / 128))
     # print("Seconds left:", str(secondsLeftInRound), tick)
     return secondsLeftInRound
 
 
-lines = sorted(open(filename).readlines(), key=lambda line: float(line.split(',')[0]))
+lines = sorted(open(csv_file).readlines(),
+               key=lambda line: float(line.split(',')[0]))
+
 for row in lines:
     tick, x, y = row.split(',')
     if (ref_tick != tick):
         if len(xcoords) > 0:
-            # first plot prev
-            # current_time = 55 - float(tick) - roundTimeOffset
+
             title = "Positions at 1:" + str(tickToRoundTime(tick) - 60)
             plt.title(title)
-            im = plt.imread("de_cache_radar.png")
-            plt.imshow(im, extent=radar_extent)
-            plt.scatter(xcoords, ycoords, marker='.', color="yellow", alpha=0.5)
-            # plt.scatter(wallbang_player_pos_x, wallbang_player_pos_y, marker='o', color="red", alpha=1)
-            # plt.plot([1000, wallbang_player_pos_x], [wallbang_player_pos_y, wallbang_player_pos_y], color="red")
-            plt.axis([-1000, 0, -500, 500])
-            plt.savefig("plots/" + "plot_" + str(imagecount).zfill(3) + ".png")
-            # if frame % frameskip == 0: plt.show()
-            # frame += 1
+            im = plt.imread("radar_images/" + radar_data.image)
+            plt.imshow(im, extent=radar_data.extent)
+            plt.scatter(xcoords, ycoords,
+                        marker='.', color="yellow", alpha=0.5)
+            plt.axis(radar_data.plotarea)
+            # plt.axis('off')
+            plt.plot([-600, 3309], [30, 69], 'k-', color='r')
+            plt.margins(0)
+            plt.savefig("plots/" + "plot_" + str(imagecount).zfill(5) + ".png",
+                        bbox_inches="tight")
             plt.gcf().clear()
             imagecount += 1
         xcoords.clear()
@@ -59,3 +108,15 @@ for row in lines:
         ref_tick = tick
     xcoords.append(float(x))
     ycoords.append(float(y))
+
+'''
+TODO:
+Plot wallbanger position and line:
+# plt.scatter(wallbang_player_pos_x,
+              wallbang_player_pos_y, marker='o', color="red", alpha=1)
+# plt.plot([1000, wallbang_player_pos_x],
+           [wallbang_player_pos_y, wallbang_player_pos_y], color="red")
+
+Last frame is not handled
+
+'''
