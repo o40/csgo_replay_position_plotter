73fb86de342b51757529e02d2e83bdac00abfb76
Daniel Lundgren <lundgren.daniel@gmail.com>
2021-07-23 11:53:10 +0200

    Adjust plotting code

--- posplotter.py
+++ posplotter.py
@@ -117,12 +117,8 @@ def save_figure(date, folder):
     filename = "{}/{}/{}.png".format(folder, date, str(g_imagecount).zfill(5))
     extent = plt.gca().get_window_extent().transformed(plt.gcf().dpi_scale_trans.inverted())
 
-    plt.gca().set_axis_off()
-    plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
-                        hspace=0, wspace=0)
+    plt.axis('off')
     plt.margins(0, 0)
-    plt.gca().xaxis.set_major_locator(plt.NullLocator())
-    plt.gca().yaxis.set_major_locator(plt.NullLocator())
     plt.savefig(filename,
                 bbox_inches='tight',
                 pad_inches=0)
